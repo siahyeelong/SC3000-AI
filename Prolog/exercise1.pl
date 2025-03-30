6bb5262c9736d63a0722467730177ca1b2e4b912
Yee Long Siah <yeelongsiah@gmail.com>
2025-03-30 16:36:40 +0800

    completed ex1 code

--- Prolog/exercise1.pl
+++ Prolog/exercise1.pl
@@ -1,29 +1,20 @@
 /* Exercise 1 */
 
 % Facts
-company(sumsum). % sumsum is a company
-company(appy). % appy is a company
-
-nice_smart_phone_tech(sumsum, galactica-s3). % sumsum produced some nice smart phone tech
-
-boss(appy, stevey). % the app of appy is stevey
-
 competitor(sumsum, appy). % sumsum is a competitor of appy
-
+competitor(appy, sumsum). % appy is also a competitor of sumsum
+develop(sumsum, galactica-s3). % sumsum produced some nice smart phone tech
+steal(stevey,galactica-s3).
+boss(appy, stevey). % the boss of appy is stevey
 
 % Rules
-competitor(X,Y) :- % if X is a competitor of Y, then Y is a competitor of X
-    company(X), % X is a company
-    company(Y), % Y is also a company
-    company(X) \= company(Y), % X and Y are not the same company
-    competitor(Y,X). % Y is also a competitor of X
-
 rival(X,Y) :- competitor(X,Y). % competitor relationship same as rivalry
 
 unethical(Person) :- % a boss is unethical if he steals business from rivals
     boss(Boss_Company,Person), % if the person in question is a boss of a company
     rival(Boss_Company, Rival_Company), % if boss's company has a rival company
-    nice_smart_phone_tech(Rival_Company, Product), % if a product belongs to the rival company
-    steal(Person, Product). % that the person has stolen
+    develop(Rival_Company, Technology), % if a technology belongs to the rival company
+    business(Technology), % if the technology is a business
+    steal(Person, Technology). % that the person has stolen
 
-steal(stevey,galactica-s3).
+business(galactica-s3).
--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # SC3000 Artificial Intelligence
 - SCSB
     - Yee Long
-    -  Xuan Ming
+    - Teo Xuan Ming
 
 This repo contains the code and deliverables for SC3000.
 
 ## Cartpole
 The `Cartpole.ipynb` file details how we have trained Reinforcement Learning agents (Monte Carlo & Q-learning) to solve the classical Cartpole problem. 
+### Usage
+Open the ipynb file in Google Colab instead. Dependencies difficult to resolve locally
 
 ## Prolog
 This folder contains implementation of simple logical agents in SWI-Prolog.
+### Usage
+1. Run swipl and cd into /Path/To/This/Repository/**Prolog**
+1. Load the exercises into swipl with `['exercise1.pl'].`
+1. Query the thing
